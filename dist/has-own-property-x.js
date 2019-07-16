4007a3d7eb89db478cca8e210ca4819345dfafc7
Graham Fairweather <xotic750@gmail.com>
2019-07-16 14:39:24 +0200

    :bookmark: v4.0.2
    Auto build

--- dist/has-own-property-x.js
+++ dist/has-own-property-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-07-16T11:00:20.996Z",
+  "date": "2019-07-16T12:39:18.793Z",
   "describe": "",
   "description": "Used to determine whether an object has an own property with the specified property key.",
   "file": "has-own-property-x.js",
   "hash": "4d7336e4634676ded567",
   "license": "MIT",
-  "version": "4.0.1"
+  "version": "4.0.2"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
